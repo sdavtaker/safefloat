330cb163c0ffae9aa5dec0b105a004d921e2a8f6
Damian Vicino <sdavtaker@gmail.com>
2015-05-03 07:36:28 +0200

    renamed safefloat to safe_float

---
+++ include/boost/safe_float.hpp
@@ -0,0 +1,13 @@
+#ifndef BOOST_SAFE_FLOAT_HPP
+#define BOOST_SAFE_FLOAT_HPP
+namespace boost {
+namespace safe_float{
+
+template<class FP, class CHECK, class ERROR_HANDLING, class CAST>
+class safe_float {
+
+};
+
+}
+}
+#endif // BOOST_SAFE_FLOAT_HPP
