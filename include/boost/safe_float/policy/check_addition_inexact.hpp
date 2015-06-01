9a3d4878e35acb97ca2c20a3e9eba3d3ec849658
Damian Vicino <sdavtaker@gmail.com>
2015-06-01 18:19:44 -0300

    introduced policy alternative on overflow and inexact for those compilers supporting FENV pragma

--- include/boost/safe_float/policy/check_addition_inexact.hpp
+++ include/boost/safe_float/policy/check_addition_inexact.hpp
@@ -2,23 +2,38 @@
 #define BOOST_SAFE_FLOAT_POLICY_CHECK_ADDITION_INEXACT_HPP
 #include <boost/safe_float/policy/check_base_policy.hpp>
 
+#ifdef FENV_AVAILABLE
+#pragma STDC FENV_ACCESS ON
+#include <fenv.h>
+#endif
+
 namespace boost {
 namespace safe_float{
 namespace policy{
 
 template<class FP>
 class check_addition_inexact : public check_policy<FP> {
+#ifndef FENV_AVAILABLE
     FP prev_l=0;
     FP prev_r=0;
+#endif
 public:
     virtual bool pre_addition_check(const FP& lhs, const FP& rhs){
+#ifndef FENV_AVAILABLE
         prev_l = lhs;
         prev_r = rhs;
         return true;
+#else
+        return ! std::feclearexcept(FE_INEXACT);
+#endif
     }
 
     virtual bool post_addition_check(const FP& rhs){
-        return (rhs - prev_r) == prev_l;
+#ifndef FENV_AVAILABLE
+        return (rhs - prev_r) == prev_l; //not sure this is enough check, need to do some math...
+#else
+        return ! std::fetestexcept(FE_INEXACT);
+#endif
     }
 
     virtual std::string addition_failure_message(){
--- include/boost/safe_float/policy/check_addition_overflow.hpp
+++ include/boost/safe_float/policy/check_addition_overflow.hpp
@@ -2,6 +2,11 @@
 #define BOOST_SAFE_FLOAT_POLICY_CHECK_ADDITION_OVERFLOW_HPP
 #include <boost/safe_float/policy/check_base_policy.hpp>
 
+#ifdef FENV_AVAILABLE
+#pragma STDC FENV_ACCESS ON
+#include <fenv.h>
+#endif
+
 namespace boost {
 namespace safe_float{
 namespace policy{
@@ -11,11 +16,19 @@ class check_addition_overflow : public check_policy<FP> {
     bool precond=true;
 public:
     virtual bool pre_addition_check(const FP& lhs, const FP& rhs){
+#ifndef FENV_AVAILABLE
         precond = std::isinf(lhs) || std::isinf(rhs);
         return true;
+#else
+        return ! std::feclearexcept(FE_OVERFLOW);
+#endif
     }
     virtual bool post_addition_check(const FP& rhs){
+#ifndef FENV_AVAILABLE
         return precond || ! std::isinf(rhs);
+#else
+        return ! std::fetestexcept(FE_OVERFLOW);
+#endif
     }
     virtual std::string addition_failure_message(){
         return std::string("Overflow to infinite on addition operation");
